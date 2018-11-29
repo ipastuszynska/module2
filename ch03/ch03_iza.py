4ee3c8a23221e76957a4423599464912c8ce16e2
Izabela Pastuszynska <ipastuszynska@gmail.com>
2018-11-29 16:44:40 +0000

    added ch03

---
+++ ch03/ch03_iza.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 29 13:55:06 2018
+
+@author: 612383386
+"""
+
+
+"""
+name = input("What's your name?")
+print  ("Hello {}!".format(name.upper()))
+
+pizza = input("What's your favourite pizza?")
+print ("{} all th way!!".format(pizza))
+
+
+animals = input("Got any animals?")
+print ("{} are the best".format(animals))
+
+flowers = input("like flowers?")
+print  ("{}".format(flowers))
+"""
+
+# upper needs extra brackets! wthout upper, the line was as below
+#print  ("Hello {}!".format(name.))
+
+
+"""
+name = input("What's your name?")
+city = input("What's your city?")
+print  ("hello {}! from{}".format(name, city))
+"""
+
+#this is the same as above but shows in two lines (no .upper applied)
+"""print ("what is your name?")
+name = input ()
+print  ("Hello {}!".format(name))"""
+
+"""
+def hello_world():
+    print ("hello world!")
+hello_world()
+
+#call the function in the console - hello_world()
+#if you want to all it hrem needs to start at the begining of the line
+
+def print_my_name ():
+    name = input("What's your name?")
+    print ("{}".format(name))
+print_my_name() 
+"""
+
+"""
+def print_my_name ():
+    name = input("What's your name?")
+    print ("{}".format(name))
+
+
+def hello_world():
+    print_my_name()
+    print ("hello world!")
+
+hello_world()
+"""
+
+
+"""    
+
+def hello_world():
+    print("Hello World")
+    
+def name_surname ():
+    print("What's your name?")
+    name = input()
+    
+    print ("My name is {}".format(name))
+    hello_world()
+    addition ()
+    
+def addition ():
+    add2_2 = 2+2 
+    print (add2_2)
+    
+name_surname()
+
+"""
+
+def print_my_name ():
+    name = input("What's your name?")
+    number = input("what's your lucky number?")
+    new_lucky_number = int(number) * 2
+    print ("{}, {} is your new lukcy number!".format(name, new_lucky_number))
+    
+print_my_name ()
+
+
+
+
