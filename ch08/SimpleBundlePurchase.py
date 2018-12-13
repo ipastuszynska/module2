cb3b802551f91b0b1574edb7fed3c523162e4d60
Iza Pastuszynska <ipastuszynska@gmail.com>
2018-12-13 13:33:34 +0000

    reorganizing files part2

--- ch08/SimpleBundlePurchase.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 11 19:23:34 2018
-
-@author: iza
-"""
-
--- ch08/ch08_iza.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 11 19:20:27 2018
-
-@author: iza
-"""
-
