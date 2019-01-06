67462285d9671048500c706d734976d8f81caa38
iza pastuszynska <ipastuszynska@gmail.com>
2019-01-06 13:16:22 +0000

    completed task 07/8 - min and max start position; created subroutine for the starting point

--- ch13_oop_project/MovingShapes.py
+++ ch13_oop_project/MovingShapes.py
@@ -15,41 +15,26 @@ class MovingShape:
         self.shape = shape
         self.diameter = diameter
         self.figure = Shape(shape,diameter)
-        self.minx = 0 + diameter/2  #07/05, 07/08
-        print("minx value: ", self.minx)
-        self.miny =  0 + diameter/2  #07/05
-        self.maxx = frame.width - diameter/2 #07/8
-        self.maxy = frame.height - diameter/2
-        self.x = self.minx + r() * (self.maxx - self.minx)
-        self.y = self.miny + r() * (self.maxy - self.miny)
-        self.goto(self.x, self.y) #07/05
+        self.startingPoint (diameter, frame.width, frame.height)
         self.startMove()
         return(self.x, self.y, self.dx, self.dy)
         
-    def startMove(self):
-#        self.x = 0  #07/05, 07/08
-#        self.y = 0   #07/05
-        
-        
+    def startingPoint (self, diameter, fwidth, fheight):
+        self.minx = 0 + diameter/2  #07/05, 07/08
+        self.miny =  0 + diameter/2  #07/05 #07/8
+        self.maxx = fwidth - diameter/2 #07/8
+        self.maxy = fheight - diameter/2 #07/8
+        self.x = self.minx + r() * (self.maxx - self.minx) #07/8
+        self.y = self.miny + r() * (self.maxy - self.miny) #07/8
+        self.goto(self.x, self.y) #07/05
+                
+    def startMove(self):        
         if r() > 0.5: #07/07
             self.dx = 5 + 10*r() #07/06
             self.dy = 5 + 10*r() #07/06 
         else: #07/07
             self.dx = -(5 + 10*r()) #07/06
             self.dy = -(5 + 10*r()) #07/06 
-            
-#    def startMove(self, diameter):     
-#        self.minx = diameter/2
-#        self.x =  self.minx + r() * (self.maxx - self.minx)
-#      
-#        self.y = 0 #07/05
-#    
-#        if r() > 0.5: #07/07
-#            self.dx = 5 + 10*r() #07/06
-#            self.dy = 5 + 10*r() #07/06 
-#        else: #07/07
-#            self.dx = -(5 + 10*r()) #07/06
-#            self.dy = -(5 + 10*r()) #07/06            
             
     def goto(self,x,y):
     
--- ch13_oop_project/test_multi_shapes.py
+++ ch13_oop_project/test_multi_shapes.py
@@ -15,7 +15,6 @@ shapes = []
 for i in range(numshapes):
     shapes.append(Square(frame,100))
 
-
 for i in range(100):
     for shape in shapes:
         shape.moveTick()
