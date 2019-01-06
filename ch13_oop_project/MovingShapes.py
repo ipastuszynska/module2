efa49771f794b86a9b364f1eb7d4961789807e50
iza pastuszynska <ipastuszynska@gmail.com>
2019-01-06 09:30:08 +0000

    completed task 07/7 - move in positive and negative directions

--- ch13_oop_project/MovingShapes.py
+++ ch13_oop_project/MovingShapes.py
@@ -15,18 +15,34 @@ class MovingShape:
         self.y = 0 #07/05
 #        self.dx = 10 #07/05
 #        self.dy = 10 #07/05
-        self.dx = 5 + 10*r() #07/06
-        self.dy = 5 + 10*r() #07/06
         self.shape = shape
         self.diameter = diameter
         self.figure = Shape(shape,diameter)
+#        self.dx = 5 + 10*r() #07/06
+#        self.dy = 5 + 10*r() #07/06 
+        
+        
+        if r() > 0.5: #07/07
+            self.dx = 5 + 10*r() #07/06
+            self.dy = 5 + 10*r() #07/06 
+        else: #07/07
+            self.dx = -(5 + 10*r()) #07/06
+            self.dy = -(5 + 10*r()) #07/06 
     def goto(self,x,y):
         self.figure.goto(x,y)
     def moveTick(self):
+#        self.x += self.dx #07/05
+#        self.y += self.dy #07/05
+#        if r() > 0.5: #07/07
+#            self.goto(self.x, self.y) #07/05
+#        else: #07/07
+#            self.goto(-(self.x), -(self.y)) #07/05
+            
+            
         self.x += self.dx #07/05
         self.y += self.dy #07/05
         self.goto(self.x, self.y) #07/05
-    
+       
 class Square(MovingShape):
     def __init__(self,frame,diameter):
         MovingShape.__init__(self,frame,'square',diameter)
