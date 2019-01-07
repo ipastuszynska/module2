a9ca87a560f33163d75c2fa0a1af7a6b15bc942b
iza pastuszynska <ipastuszynska@gmail.com>
2019-01-07 10:29:12 +0000

    Finished  Tasks 1-6 in ch13, delted backup files for ch13 project

--- ch13_oop_project/ch13_oop_project_iza.py
+++ ch13_oop_project/ch13_oop_project_iza.py
@@ -39,55 +39,93 @@ Created on Thu Jan  3 17:32:45 2019
 #● Task 7.4 Testing
 #-----------------------------------------------------
     
-#    
-#class Person:
-#    def __init__ (self, name, age, gender):
-#        self.name = name
-#        self.age = age
-#        if gender == "m":
-#            self.isMale = True
-#        elif gender == "f":
-#            self.isMale = False
-#        else:
-#            print("Gender not recognized")
-#    def greetingInformal (self):
-#        print ("Hi", self.name)
-#    def greetingFormal (self):
-#        if self.isMale:
-#            print ("Welcome Mr.", self.name)
-#        else:
-#            print ("Welcome Ms.", self.name)
-##cerate objects 
-#            
-#p1 = Person ("John", 19, "m")
-#p2 = Person ("Jo", 18, "f")
-#print (p1)
-#print (p1.name)
-#
-#p1.greetingFormal()
-#
-## subclasss for person
-#
-#class Wizard(Person):
-#    def greetingFormal(self):
-#        print("Welcome", self.name, end="")
-#        print (" you are a wizard!")
-#        
-#p3 = Wizard ("Jay", 31, "m")
-#p3.greetingFormal()
+#-----------------------------------------------------
+#TASK 1: Initialise the person class
+#TASK2: More functionalities for the Person class (formal and informal greeting)
+#TASK3: A greeting filter (age based)
+#-----------------------------------------------------
 
-from Shapes import*
 
-##ch13 project
-frame = Frame()
-s1 = Shape('square',100)
-s1.goto(200,100)
+class Person:
+    def __init__ (self, name, age, gender):
+        self.name = name
+        self.age = age
+        if gender == "m":
+            self.isMale = True
+        elif gender == "f":
+            self.isMale = False
+        else:
+            print("Gender not recognized")
+    def greetingInformal (self):
+        print ("Hi", self.name)
+    def greetingFormal (self):
+        if self.isMale:
+            print ("Welcome Mr.", self.name)
+        else:
+            print ("Welcome Ms.", self.name)
+    def greetingAgeBased(self):
+        if self.age < 18:
+            print('Welcome, young ', self.name)
+        elif self.age > 60:
+            print('Welcome, venerable ', self.name)
+        else:
+            self.greetingFormal()
 
-x=0
-y=0
-for i in range(100):
-    s1.goto(x,y)
-    x = x + 5
-    y = y + 5
-#turtle.done()
-frame.close()
+p1 = Person ("John", 19, "m")
+p2 = Person ("Jo", 18, "f")
+print (p1)
+print (p1.name)
+
+p1.greetingFormal()
+
+#-----------------------------------------------------
+#TASK 4: Create a subclass for the Person class 
+#TASK 5: Redefine init        
+#TASK 6: Add new methods to the Wizard class
+#-----------------------------------------------------
+
+
+class Wizard(Person):
+    def __init__(self,name,age,gender):
+        Person.__init__(self,name,age,'m')
+    def greetingFormal(self):
+        print("Welcome", self.name, end="")
+        print (" you are a wizard!")
+    def someMagic(self):
+        print ("Abracadabra")
+
+print("p3 = Wizard ('Jay', 31, 'm'), greeting formal, greeting age based, someMagic")
+print()   
+p3 = Wizard ("Jay", 31, "m")
+p3.greetingFormal()
+p3.greetingAgeBased()
+p3.someMagic()
+print()
+
+print("p4 = Person ('Padawan', 13, 'm'),  greeting age based")
+print()
+p4 = Person ("Padawan", 13, "m")
+p4.greetingAgeBased()
+print()
+
+print("p5 = Person ('Yoda', 13, 'm'),  greeting age based")
+print()
+p5 = Person ("Yoda", 65, "m")
+p5.greetingAgeBased()
+print()
+
+#from Shapes import*
+#
+###ch13 project
+#frame = Frame()
+#s1 = Shape('square',100)
+#s1.goto(200,100)
+#
+#x=0
+#y=0
+#for i in range(100):
+#    s1.goto(x,y)
+#    x = x + 5
+#    y = y + 5
+##turtle.done()
+#frame.close()
